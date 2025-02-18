832cfc5373fd97f942ee97b5253276afd489aaa1
Chustz, Joseph <Joseph.Chustz@brownandroot.com>
2025-02-18 12:49:33 -0500

    added mssql query function

---
+++ src/database/mssql/executeSQLQuery.ts
@@ -0,0 +1,34 @@
+import * as sql from 'mssql';
+import { z } from 'zod';
+import { validateWithZod } from '../../utilities/zodUtility';
+
+export interface MSSQLDBConfig {
+  user: string;
+  password: string;
+  server: string;
+  database: string;
+}
+
+const validationSchema = z.object({
+  query: z.string().min(10, 'Query length is not long enough'),
+});
+
+export async function executeSqlQuery(dbConfig: MSSQLDBConfig, query: string) {
+  const validationResult = validateWithZod(validationSchema, { query });
+
+  if (validationResult.isError) {
+    throw new Error(`Execute Sql Query validation failed: ${JSON.stringify(validationResult.error)}`);
+  }
+
+  const configWithCertOptions = {
+    ...dbConfig,
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+    },
+  };
+
+  await sql.connect(configWithCertOptions);
+  const sqlResponse = await sql.query(query);
+  return sqlResponse.recordset;
+}
