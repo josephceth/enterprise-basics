3be0c806f9b008c0ecfcc789d521f55f244dd4e2
Chustz, Joseph <Joseph.Chustz@brownandroot.com>
2025-02-27 11:23:46 -0500

    added jsdoc to getENVValue

--- src/fileSystem/getEnvValue.ts
+++ src/fileSystem/getEnvValue.ts
@@ -5,10 +5,25 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export function getEnvValue(key: string): string {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`Environment variable ${key} is not set in the .env file or environment variables`);
-  }
-  return value.toString();
+/**
+ * Asynchronously retrieves the value of an environment variable
+ * @param {string} key - The name of the environment variable to retrieve
+ * @returns {Promise<string>} A promise that resolves with the value of the environment variable
+ * @throws {Error} If the environment variable is not set, undefined, or null
+ * @example
+ * try {
+ *   const value = await getEnvValue('API_KEY');
+ *   console.log(value);
+ * } catch (error) {
+ *   console.error(error);
+ * }
+ */
+export async function getEnvValue(key: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const value = process.env[key];
+    if (!value || value === undefined || value === null) {
+      reject(new Error(`Environment variable ${key} is not set in the .env file or environment variables`));
+    }
+    resolve((value as string).toString());
+  });
 }
